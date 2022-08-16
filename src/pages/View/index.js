9132a8c32680f271773be087a0ce6a2f11422aa0
amandeep2508 <amandeep.chauhan@cogoport.com>
2022-08-16 20:36:35 +0530

    added view section

--- src/pages/View/index.js
+++ src/pages/View/index.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { toast } from "react-toastify"
+import { useParams } from "react-router-dom"
+import "./styles.css"
 
 const View = () => {
+  const [data, setData] = useState({})
+
+  const { id } = useParams()
+
+  const getuser = async (id) => {
+    const res = await axios.get(`http://localhost:5000/user/${id}`)
+
+    if (res.status === 200) {
+      setData(res.data[0])
+    } else {
+      toast.error("User not found")
+    }
+  }
+
+  useEffect(() => {
+    if (id) {
+      getuser(id)
+    }
+  }, [id])
+
   return (
-	<div>View</div>
+    <div className="view">
+      <div className="view-group">
+        <h3>Name: </h3>
+        <span>{data.name}</span>
+      </div>
+      <div className="view-group">
+        <h3>Email: </h3>
+        <span>{data.email}</span>
+      </div>
+      <div className="view-group">
+        <h3>Contact: </h3>
+        <span>{data.contact}</span>
+      </div>
+    </div>
   )
 }
 
